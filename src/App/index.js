6777da74e2037c2e29778311855a1ee721dbf2cc
Rachel Ruderman <raquel.rudermano@gmail.com>
2020-07-04 16:57:19 -0700

    Tweak base CSS

--- src/App/index.js
+++ src/App/index.js
@@ -5,17 +5,25 @@ export const App = () => {
   const renderHeader = () => {
     return (
       <header>
-        <h1>Low on fuel?</h1>
-        <h3>Find your nearest gas stations</h3>
+        <h1>Running on empty?</h1>
+        <h3>Take a break, grab a coffee and fuel up!</h3>
       </header>
     )
   }
 
   const renderBody = () => {
     return (
-      <div>
-        <button>Find local gas stations</button>
+      <div id='body'>
+        <button type='button'>Find local gas stations</button>
       </div>
+    )
+  }
+
+  const renderFooter = () => {
+    return (
+      <footer>
+        i am footer
+      </footer>
     )
   }
 
@@ -23,11 +31,9 @@ export const App = () => {
     <div id="app">
       <div id='content'>
         {renderHeader()}
-
-
-
+        {renderBody()}
+        {renderFooter()}
       </div>
-
     </div>
   );
 }
