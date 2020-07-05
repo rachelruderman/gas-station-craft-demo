6fbe317ef988c7875c4624a7b82c18d25ec02093
Rachel Ruderman <raquel.rudermano@gmail.com>
2020-07-04 18:30:55 -0700

    Add state

--- src/App/Cta/index.js
+++ src/App/Cta/index.js
@@ -1,19 +1,42 @@
 import React, { useState } from 'react';
-import { INITIAL, CLICKED } from './_util/enums';
+import { INITIAL, FETCHING_LOCATION, ERROR } from './_util/enums';
 import { buttonStates } from './_util/buttonStates';
 
 export const Cta = () => {
 
-    const [buttonState, setButtonState] = useState(INITIAL);
+    const initialButtonState = INITIAL;
+    const initialPostalCode = undefined;
+    
+    const [buttonState, setButtonState] = useState(initialButtonState);
+    const [postalCode, setPostalCode] = useState(initialPostalCode);
 
     const { text } = buttonStates.find( ({state}) => (state === buttonState));
 
+
+    const getPostalCode = () => {
+        const { geolocation } = navigator;
+        const isGeolocationSupported = Boolean(geolocation);
+
+        if (isGeolocationSupported) {
+            geolocation.getCurrentPosition( (position) => {
+                console.log({position})
+            })
+        }
+
+        return setButtonState(ERROR);
+    }
+
+    const onClick = () => {
+        // pointer events are only enabled in INITIAL state
+        setButtonState(FETCHING_LOCATION);
+        const postalCode = getPostalCode();
+    }
     return (
         <button
             type='button'
             id='cta'
             className={buttonState}
-            onClick={() => setButtonState(CLICKED)}
+            onClick={onClick}
         >
             {text}
         </button>
