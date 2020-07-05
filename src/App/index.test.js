f89b4a69c3bd248f69a4f3452446cd167984d8ae
Rachel Ruderman <raquel.rudermano@gmail.com>
2020-07-05 09:31:22 -0700

    Add copyright test

--- src/App/index.test.js
+++ src/App/index.test.js
@@ -4,6 +4,14 @@ import { App } from './index';
 
 test('renders Clearbit attribution', () => {
   const { getByText } = render(<App />);
-  const attributionEl = getByText(/Logos provided by Clearbit/i);
-  expect(attributionEl).toBeInTheDocument();
+  const element = getByText(/Logos provided by Clearbit/i);
+  expect(element).toBeInTheDocument();
 });
+
+test('renders Fuel Up copyright', () => {
+  const { getByText } = render(<App />);
+  // todo: make dynamic w/ current year
+  const element = getByText(/Fuel Up © 2020/i);
+  expect(element).toBeInTheDocument();
+});
+
