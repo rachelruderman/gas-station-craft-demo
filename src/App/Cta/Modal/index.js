0fbfc9d577fdafb702c4c709e3644da726343d75
Rachel Ruderman <raquel.rudermano@gmail.com>
2020-07-04 22:20:53 -0700

    Use table instead of ul

--- src/App/Cta/Modal/index.js
+++ src/App/Cta/Modal/index.js
@@ -4,7 +4,7 @@ export const Modal = (props) => {
 
     const [page, setPage] = useState(0);
 
-    const stationsPerPage = 7;
+    const stationsPerPage = 5;
 
     const {
         gasStations: allGasStations,
@@ -15,30 +15,51 @@ export const Modal = (props) => {
     console.log({gasStations})
 
     return (
-        <ul id='cta'>
-            {gasStations.map(gasStation => {
-                const { station, id, address, distance, city, zip, country} = gasStation;
-                
-                const name = (station === 'Unbranded')
-                    ? `There's one`
-                    : `${station} is`;
-                
-                const miles = (distance === '1 miles')
-                    ? '1 mile'
-                    : distance;
+        <table id='cta'>
+            <thead>
+                <tr>
+                    <td>Distance</td>
+                    <td>Station</td>
+                    <td>Gas Price</td>
+                </tr>
+            </thead>
+            <tbody>
+                {gasStations.map(gasStation => {
+                    const {
+                        station,
+                        id,
+                        address,
+                        distance,
+                        city,
+                        zip,
+                        country,
+                        reg_price
+                    } = gasStation;
+                    
+                    const name = (station === 'Unbranded')
+                        ? `Mystery station`
+                        : station;
+                    
+                    const miles = (distance === '1 miles')
+                        ? '1 mile'
+                        : distance;
 
-                const urlEscapedAddress = `${address} ${city} ${zip} ${country}`.replace(/ /g, '+');
-                const href = `https://www.google.com/maps/dir/?api=1&destination=${urlEscapedAddress}&travelmode=driving`;
+                    const urlEscapedAddress = `${address} ${city} ${zip} ${country}`.replace(/ /g, '+');
+                    const href = `https://www.google.com/maps/dir/?api=1&destination=${urlEscapedAddress}&travelmode=driving`;
 
-                return (
-                    <li key={id}>
-                        {name} {miles} away at{` `}
-                        <a href={href} alt='directions' target='_blank' rel='noopener noreferrer'>
-                            {address}
-                        </a> 
-                    </li>
-                )
-            })}
-        </ul>
+                    return (
+                        <tr key={id}>
+                            <td>{miles}</td>
+                            <td>
+                                <a href={href} alt='directions' target='_blank' rel='noopener noreferrer'>
+                                    {name}
+                                </a>
+                            </td>
+                            <td>${reg_price}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
     )
 }
